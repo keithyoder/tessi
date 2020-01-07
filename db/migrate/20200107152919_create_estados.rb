5338af5a5a9a3e366989315bcc5f493c36f718ed
Keith Yoder <yoder@tessi.com.br>
2020-01-07 14:01:04 -0300

    add Estado model

---
+++ db/migrate/20200107152919_create_estados.rb
@@ -0,0 +1,11 @@
+class CreateEstados < ActiveRecord::Migration[5.2]
+  def change
+    create_table :estados do |t|
+      t.string :sigla
+      t.string :nome
+      t.integer :ibge
+
+      t.timestamps
+    end
+  end
+end
