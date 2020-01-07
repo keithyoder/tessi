81a85785f5423647525ebdda04f0064d1b53c6b9
Keith Yoder <yoder@tessi.com.br>
2020-01-07 17:15:26 -0300

    attemt to fix seed

--- config/deploy.rb
+++ config/deploy.rb
@@ -53,9 +53,13 @@ set :keep_releases, 5
 # This is useful if you don't want to use ENV variables
 # append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
-namespace :deploy do
-    desc "reload the database with seed data"
-    task :seed do
-      run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+desc 'Runs rake db:seed'
+task :seed => [:set_rails_env] do
+  on primary fetch(:migration_role) do
+    within release_path do
+      with rails_env: fetch(:rails_env) do
+        execute :rake, "db:seed"
+      end
     end
   end
+end
