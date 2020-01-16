6d1cf148dd64395b7208a5fbd684615101d92340
Keith Yoder <yoder@tessi.com.br>
2020-01-16 20:02:59 -0300

    fix mikrotik

--- app/models/servidor.rb
+++ app/models/servidor.rb
@@ -1,7 +1,8 @@
 class Servidor < ApplicationRecord
   def ppp_users
     begin
-      users = MTik::command(:host => self.ip.to_s, :user => self.usuario, :pass => self.senha, :unencrypted_plaintext => :true, :command => '/ppp/active/print').to_json
+      users = MTik::command(:host => self.ip.to_s, :user => self.usuario,
+        :pass => self.senha, :unencrypted_plaintext => :true, :command => '/ppp/active/print')
       users[0].count.to_s
     rescue StandardError => e  
       e.message
