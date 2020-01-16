87aad07e0986b323ff6aea19c6439dc1e9d02d9e
Keith Yoder <yoder@tessi.com.br>
2020-01-16 19:46:00 -0300

    add mikrotik

--- app/models/servidor.rb
+++ app/models/servidor.rb
@@ -1,9 +1,11 @@
 class Servidor < ApplicationRecord
-  def uptime
-    tk = MTik::Connection.new(:host => self.ip.to_s, :user => self.usuario, :pass => self.senha)
-    tk.fetch(ARGV[3]) do |status, total, bytes|
-      puts "fetch status: #{status} -- #{bytes} bytes of #{total} downloaded..."
+  def ppp_users
+    begin
+      users = MTik::command(:host => '10.200.8.34', :user => 'admin', :pass => '010407kry', :unencrypted_plaintext => :true, :command => '/ppp/active/print').to_json
+      users[0].count
+    rescue
+      puts "Error"
     end
-    tk.close
   end
+
 end
