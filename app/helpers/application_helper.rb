ac0e3b52d61d0af3d06392c818a9222126d02786
Keith Yoder <yoder@tessi.com.br>
2020-01-16 20:40:06 -0300

    add ping

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,14 +2,26 @@ module ApplicationHelper
   def new_table_header(path)
     link_to '<i class="fa fa-plus" aria-hidden="true"></i>'.html_safe, path, class: "btn btn-sm btn-outline-dark"
   end
+
   def edit_button(path)
     link_to '<i class="fa fa-pencil fa-lg" aria-hidden="true"></i>'.html_safe, path, class: "btn btn-sm btn-outline-dark"
   end
+
   def index_button(path)
     link_to '<i class="fa fa-arrow-left" aria-hidden="true"></i>'.html_safe, path, class: "btn btn-sm btn-outline-dark"
   end
+
   def delete_button(path)
     link_to '<i class="fa fa-trash-o fa-lg" aria-hidden="true"></i>'.html_safe, path, method: :delete,
       class: "btn btn-sm btn-danger", data: { confirm: 'Você tem certeza?' }
   end
+
+  def online_button(up)
+    if up
+      "btn-success"
+    else
+      "btn-danger"
+    end
+  end
+
 end
--- app/models/servidor.rb
+++ app/models/servidor.rb
@@ -3,10 +3,14 @@ class Servidor < ApplicationRecord
     begin
       users = MTik::command(:host => self.ip.to_s, :user => self.usuario,
         :pass => self.senha, :unencrypted_plaintext => :true, :command => '/ppp/active/print')
-      users[0].count.to_s
+      (users[0].count - 1).to_s
     rescue StandardError => e  
       e.message
     end
   end
 
+  def up?
+    check = Net::Ping::External.new(self.ip.to_s)
+    check.ping?
+  end
 end
