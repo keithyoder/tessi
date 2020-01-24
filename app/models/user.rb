9b0f77952cbf9721fc45664f626b521a0fe8ed05
Keith Yoder <yoder@tessi.com.br>
2020-01-24 16:44:08 -0300

    add age

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  enum role: [:admin, :tecnico_n1, :tecnico_n2, :financeiro_n1, :financeiro_n2]
+  enum role: [0 => :admin, 1 => :tecnico_n1, 2 => :tecnico_n2, 3 =>:financeiro_n1, 4 => :financeiro_n2]
 end
