1a904959816e683c90340749879c8cc99cdfc94d
Keith Yoder <yoder@tessi.com.br>
2020-02-05 15:25:56 -0300

    more liquidacoes views

--- app/controllers/liquidacoes_controller.rb
+++ app/controllers/liquidacoes_controller.rb
@@ -2,7 +2,15 @@ class LiquidacoesController < ApplicationController
     load_and_authorize_resource :fatura, :through => :liquidacoes 
 
   def index
-    @liquidacoes = Fatura.select("liquidacao as data, count(*) as liquidacoes, sum(valor_liquidacao) as valor").group(:liquidacao).order(liquidacao: :desc).page params[:page]
+    @liquidacoes = Fatura.select("date(liquidacao) as data, count(*) as liquidacoes, sum(valor_liquidacao) as valor")
+        .where("liquidacao not null")
+        .group(:liquidacao)
+        .order(liquidacao: :desc)
+        .page params[:page]
   end
     
+  def show
+    @liquidacoes = Fatura.where("liquidacao = ?", params[:id]).page params[:page]
+  end
+
 end
