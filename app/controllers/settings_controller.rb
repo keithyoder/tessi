5e8ad1c70f432d96b80e62c8c22a903ed86ce778
Keith Yoder <yoder@tessi.com.br>
2020-02-12 12:23:52 -0300

    backup refactor

---
+++ app/controllers/settings_controller.rb
@@ -0,0 +1,18 @@
+class SettingsController < ApplicationController
+  load_and_authorize_resource
+  before_action :get_setting, only: [:edit, :update]
+
+  def create
+    puts setting_params.to_s
+    setting_params.keys.each do |key|
+      Setting.send("#{key}=", setting_params[key].strip) unless setting_params[key].nil?
+    end
+    redirect_to settings_path, notice: "Configurações atualizadas."
+  end
+
+  private
+
+  def setting_params
+    params.require(:setting).permit(:razao_social, :cnpj, :juros, :multa)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
   }
   resources :estados
   resources :token
+  resources :settings
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
