361c2485180d9c63d62f22648a714b2d46f1a3be
Keith Yoder <yoder@tessi.com.br>
2021-02-23 08:56:54 -0300

    santander

--- app/models/pagamento_perfil.rb
+++ app/models/pagamento_perfil.rb
@@ -1,10 +1,40 @@
 class PagamentoPerfil < ApplicationRecord
   has_many :faturas
   has_many :retornos
-  enum tipo: { "Boleto" => 3, "Débito Automático" => 2 }
+  enum tipo: { 'Boleto' => 3, 'Débito Automático' => 2 }
 
   def remessa(pagamentos)
-    info = {
+    case banco
+    when 33
+      remessa_santander(pagamentos)
+    when 1
+      remessa_banco_brasil(pagamentos)
+    end
+  end
+
+  private
+
+  def remessa_banco_brasil(pagamentos)
+    Brcobranca::Remessa::Cnab400::BancoBrasil.new(
+      remessa_attr(pagamentos).merge(
+        variacao_carteira: variacao.to_s,
+        convenio: cedente.to_s,
+        convenio_lider: cedente.to_s
+      )
+    )
+  end
+
+  def remessa_santander(pagamentos)
+    Brcobranca::Remessa::Cnab400::Santander.new(
+      remessa_attr(pagamentos).merge(
+        codigo_transmissao: santander_codigo_transmissao,
+        codigo_carteira: variacao.to_s,
+      )
+    )
+  end
+
+  def remessa_attr(pagamentos)
+    {
       carteira: carteira.to_s,
       agencia: agencia.to_s,
       conta_corrente: conta.to_s,
@@ -14,26 +44,9 @@ class PagamentoPerfil < ApplicationRecord
       documento_cedente: Setting.cnpj,
       pagamentos: pagamentos,
     }
-    case banco
-    when 33
-      Brcobranca::Remessa::Cnab400::Santander.new(
-        info.merge(
-          {
-            codigo_transmissao: (agencia.to_s + '0' + cedente.to_s + conta.to_s)[0...20],
-            codigo_carteira: variacao.to_s,
-          }
-        )
-      )
-    when 1
-      Brcobranca::Remessa::Cnab400::BancoBrasil.new(
-        info.merge(
-          {
-            variacao_carteira: variacao.to_s,
-            convenio: cedente.to_s,
-            convenio_lider: cedente.to_s
-          }
-        )
-      )
-    end
+  end
+
+  def santander_codigo_transmissao
+    (agencia.to_s + '0' + cedente.to_s + conta.to_s.rjust(10, '0'))[0...20]
   end
 end
