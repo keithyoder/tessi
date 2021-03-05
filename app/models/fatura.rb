a06b7d282c24129225928fd515ff0cd46d8106d5
Keith Yoder <yoder@tessi.com.br>
2021-03-05 11:03:18 -0300

    allow old payments

--- app/models/fatura.rb
+++ app/models/fatura.rb
@@ -178,7 +178,8 @@ class Fatura < ApplicationRecord
 
   def validar_liquidacao?
     return if liquidacao.blank? || (1.week.ago..Date.today).cover?(liquidacao)
+    return if meio_liquidacao == :Outros
 
-    errors.add(:liquidacao, "fora da faixa permitida")
+    errors.add(:liquidacao, 'fora da faixa permitida')
   end
 end
