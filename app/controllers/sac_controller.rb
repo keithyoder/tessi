368233d57188f98022c56b103b864c23f8e55279
Keith Yoder <yoder@tessi.com.br>
2021-04-08 10:27:46 -0300

    logging

--- app/controllers/sac_controller.rb
+++ app/controllers/sac_controller.rb
@@ -11,6 +11,7 @@ class SacController < ApplicationController
   private
 
   def set_conexao
+    puts request
     ip = request.env['HTTP_X_FORWARDED_FOR'] || request.remote_ip
     @conexao = Conexao.find_by(ip: ip)
     # @conexao = Conexao.find_by(ip: '10.36.1.107')
