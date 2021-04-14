627c7a6cd92419bcdcbe3d869dea84f98df35100
Keith Yoder <yoder@tessi.com.br>
2021-04-14 08:56:38 -0300

    validar que atendimento nao tem dados de cartao

--- app/models/atendimento_detalhe.rb
+++ app/models/atendimento_detalhe.rb
@@ -9,4 +9,14 @@ class AtendimentoDetalhe < ApplicationRecord
     Facebook: 4,
     Email: 5,
   }
+
+  validate :nao_contem_dados_de_cartao?
+
+  private
+
+  def nao_contem_dados_de_cartao?
+    return unless descricao.gsub(/[^a-zA-Z0-9]/, '').match?(/\d{13,16}/)
+
+    errors.add(:descricao, 'não pode ter dados de cartão')
+  end
 end
