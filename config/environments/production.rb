27797d5e9db468013db3ba4698d0b3cf7d23a53f
Keith Yoder <yoder@tessi.com.br>
2021-05-20 11:26:58 -0300

    Revert "precompile"
    This reverts commit d9652ed4c846968457c627c4bbd6f766125afef5.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  # config.assets.precompile += ['*.js']
+  config.assets.precompile += ['*.js', '*.coffee']
 
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
