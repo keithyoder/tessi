3065de4e67b2b32df9101ee4672f1c52f89b3e90
Keith Yoder <yoder@tessi.com.br>
2021-12-23 15:55:06 -0300

    remove creds

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -4,8 +4,8 @@ ActionMailer::Base.smtp_settings = {
   address: 'smtp.gmail.com',
   port: 587,
   domain: 'tessi.com.br',
-  user_name: 'yoder@tessi.com.br',
-  password: 'Fusca1977',
+  user_name: 'XXXXX',
+  password: 'XXXXX',
   authentication: 'plain',
   enable_starttls_auto: true
 }
