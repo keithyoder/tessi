3ede380063f136ec079ba09feb9657060f32df09
keithyoder <yoder@tessi.com.br>
2022-06-13 16:43:56 -0300

    peform_in 10 seconds

--- app/jobs/gerencianet_boleto_job.rb
+++ app/jobs/gerencianet_boleto_job.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class GerencianetBoletoJob < ApplicationJob
-    queue_as :default
+  include Sidekiq::Worker
   
-    def perform(fatura)
-      GerencianetClient.criar_boleto(fatura)
-    end
+  def perform(fatura)
+    GerencianetClient.criar_boleto(fatura)
   end
+end
   
