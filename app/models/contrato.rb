94d023a0c46d20c6b5b1fa9952df32331e0a69a4
keithyoder <yoder@tessi.com.br>
2022-07-18 07:26:48 -0300

    update renovaveis

--- app/models/contrato.rb
+++ app/models/contrato.rb
@@ -47,9 +47,10 @@ class Contrato < ApplicationRecord
       .group('contratos.id, pessoas.id, planos.id')
       .having('COUNT(faturas.*) > 4').ativos
   }
+
   scope :renovaveis, lambda {
     joins(:pessoa, :plano)
-      .joins("LEFT JOIN faturas ON contratos.id = faturas.contrato_id AND faturas.periodo_fim > '#{30.days.from_now}'")
+      .joins("LEFT JOIN faturas ON contratos.id = faturas.contrato_id AND faturas.periodo_fim > '#{15.days.from_now}'")
       .group('contratos.id', 'pessoas.id', 'planos.id')
       .having('COUNT(faturas.*) = 0').ativos
   }
