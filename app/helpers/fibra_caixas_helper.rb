816e96b60356eaf34ad8ace8bd396b807828d867
keithyoder <yoder@tessi.com.br>
2022-10-25 10:17:41 -0300

    update caixa map

--- app/helpers/fibra_caixas_helper.rb
+++ app/helpers/fibra_caixas_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module FibraCaixasHelper
+  def markers
+    markers = ["markers=color:blue%7Clabel:C|#{@fibra_caixa.latitude},#{@fibra_caixa.longitude}"]
+    @fibra_caixa.conexoes.where.not(latitude: nil, longitude: nil).each do |c|
+      markers.append("markers=label:#{c.porta}|#{c.latitude},#{c.longitude}")
+    end
+    markers.join('&')
+  end
 end
