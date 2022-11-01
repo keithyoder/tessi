0449c5e366d5a31f579e9bc654bd5c7fee6aedc1
keithyoder <yoder@tessi.com.br>
2022-11-01 07:49:32 -0300

    fix fatura valor

--- app/models/fatura.rb
+++ app/models/fatura.rb
@@ -301,7 +301,7 @@ class Fatura < ApplicationRecord
             periodo_fim
           end
     dias_no_mes = fim - (fim - 1.month)
-    dias_no_periodo = if (periodo_fim.end_of_month == periodo_fim) && (periodo_fim.day != periodo_inicio.day)
+    dias_no_periodo = if (periodo_fim.end_of_month == periodo_fim) && (periodo_fim.day != periodo_inicio.day) && (periodo_fim.month != periodo_inicio.month)
                         (periodo_fim - periodo_inicio.end_of_month).to_f
                       else
                         (periodo_fim - periodo_inicio).to_f + 1
